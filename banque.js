69a44d590cf78dad898a36736a928da118c2e08d
Fabien <fab.arnal@free.fr>
2022-11-25 11:27:20 +0100

    ajout fonction verifcompte + valeur champ compte

---
+++ banque.js
@@ -0,0 +1,9 @@
+
+/*création de la fonction de vérification de compte*/
+function Verifcompte(){
+
+    /*récupération de la valeur de l'input compte*/
+    const compteInput = document.getElementById("compte").value;
+
+
+}
