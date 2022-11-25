a679f9acd86c550008bb2ab11e7a816f57d5a7d6
Fabien <fab.arnal@free.fr>
2022-11-25 11:59:44 +0100

    update verif compte try chgmt couleur background si 16 chiffres

--- banque.js
+++ banque.js
@@ -6,26 +6,37 @@ function Verifcompte(){
     var compteInput = document.getElementById("compte").value;
 
     /*test valeur récupérée*/
-    console.log(compteInput);
-    alert(compteInput);
+    //console.log(compteInput);
+    //alert(compteInput);
 
     /*condition si num compte est composé de 16 chiffres*/
     if(compteInput.length === 16){
-        return compteInput.value;
+        const input = document.getElementById('compte');
+
+        input.addEventListener('input', event => {
+        if (input.length === 16) {
+            input.style.backgroundColor = '#6eff33';
+        } else {
+            input.style.backgroundColor = '';
+        }
+        });
     }
 }
 
+
 /*création de la fonction de vérification de la somme à déposer*/
-function VerifSomme(){
+function Verifsomme(){
     /*récupération de la valeur de l'input somme*/
     var sommeInput = document.getElementById("somme").value;
 
     /*test valeur récupérée*/
-    console.log(sommeInput);
-    alert(sommeInput);
+    //console.log(sommeInput);
+    //alert(sommeInput);
 
     /*condition si somme ets sup à 0*/
     if(sommeInput.value > 0){
         return sommeInput.value;
     }
 }
+
+Verifcompte();
