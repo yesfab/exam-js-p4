88c10a9b7867f7ef623f1742957e49914a54622d
Fabien <fab.arnal@free.fr>
2022-11-25 11:33:57 +0100

    ajout condition si num compte égal à 16 chiffres

--- banque.js
+++ banque.js
@@ -5,5 +5,11 @@ function Verifcompte(){
     /*récupération de la valeur de l'input compte*/
     const compteInput = document.getElementById("compte").value;
 
+    console.log(compteInput);
+
+    /*condition si num compte est composé de 16 chiffres*/
+    if(compteInput.length === 16){
+        return compteInput;
+    }
 
 }
